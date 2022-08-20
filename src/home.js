c5e23d18ee99c1fc499fa8796a758254865abe18
HENILCODES <prajapatihenil1112@gmail.com>
2022-08-20 19:38:46 +0530

    edit some bug

--- src/home.js
+++ src/home.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
-
 export default function Textfun() {
   const [Text, setText] = useState("");
 
   // handel textarea
-  const textareaClick = (even) => {
-    setText(even.target.value);
+  const textareaClick = (event) => {
+    setText(event.target.value);
   }
 
   //Count Lenght
@@ -14,9 +13,10 @@ export default function Textfun() {
 
   //Count Word
   let WordCount = Text.split(' ').filter((n)=>{return n !== ''}).length;
+  // WordCount.toString().split(' ,')
 
   // Count Character
-  let CharacteCount = Text.trim().split('').filter((n)=>{return n !==''}).length;
+  let CharacteCount = Text.trim().split('').filter((n)=>{return n !==' '}).length;
 
   // Upppercase
   let upperCase = ()=>{
@@ -32,15 +32,25 @@ export default function Textfun() {
   
   // capitlize
   let capitalizeCase = ()=>{
-    let toCapit= Text.charAt(0).toUpperCase() + Text.slice(1);
-    // console.log(toCapit);
-    setText(toCapit);
+    let toSplit= Text.split(' ');
+    let strin = toSplit.map((n)=>{
+      return n.charAt(0).toUpperCase() + n.slice(1);
+    })
+    let realCapital = strin.toString();
+    console.log(realCapital);
+    setText(realCapital);
   }
 
+  // Copy Text
+  let copyText = ()=>{
+    var CopyTexT = document.getElementById("Textarea");
+    CopyTexT.select();
+    navigator.clipboard.writeText(CopyTexT.value);
+  }
 
   return (
     <div className="pt-5 w-75 m-auto">
-      <div className="form-floating my-5">
+      <div className="my-5">
         <textarea
           className="form-control border border-info bg-info text-wrap bg-opacity-10 rounded-end"
           placeholder="Type Word"
@@ -49,29 +59,27 @@ export default function Textfun() {
           value={Text}
           onChange={textareaClick}
         ></textarea>
-        <label htmlFor="floatingTextarea2" className="fw-normal">
-          Enter Text
-        </label>
+        
         <div className="group mt-2 opacity-50 fs-5">
           <div className="d-flex flex-wrap">
             <div className="mx-3">
               {" "}
-              Character :{" "}
+              Character {" "}
               <span className="badge rounded-5 bg-dark">{CharacteCount}</span>
             </div>
             <div className="mx-3">
               {" "}
-              Word : <span className="badge rounded-4 bg-dark">{WordCount} </span>
+              Word <span className="badge rounded-4 bg-dark">{WordCount} </span>
             </div>
             <div className="mx-3">
               {" "}
-              Length :{" "}
+              Length {" "}
               <span className="badge rounded-5 bg-dark">{TextLength} </span>
             </div>
           </div>
         </div>
       </div>
-      <div className="d-flex flex-wrap">
+      <div className="d-flex flex-wrap container">
         <div className="m-2">
           <button type="button gap-3" className="btn btn-primary" onClick={upperCase}>
             UpperCase
@@ -85,6 +93,16 @@ export default function Textfun() {
         <div className="m-2">
           <button type="button gap-3" className="btn btn-primary" onClick={capitalizeCase}>
             Capitlize
+          </button>
+        </div>
+        <div className="m-2">
+          <button type="button gap-3" className="btn btn-primary" onClick={copyText}>
+            Copy
+          </button>
+        </div>
+        <div className="m-2">
+          <button type="button gap-3" className="btn btn-danger" onClick={()=>{setText('')}}>
+            Clear
           </button>
         </div>
       </div>
