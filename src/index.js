e5e594a77de27a6b7fe75e2fe92ed8459ae11895
henilcode <henilcode@gmail.com>
2022-12-07 15:10:32 +0530

    start First Chat App Code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Header,Social} from './header';
-import Navbar from './navbar';
-import {About,Education,Knowledge} from './content';
-import { Project } from './project';
-import './CSS/main.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const name = "Henil Code";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Social nikename={name}/>
-    <Header name="Henil Prajapati"/>
-    <Navbar />
-    <About email="henilcode@gmail.com" address="Vapi Gunjan" number="9081807209" />
-    <Education/>
-    <Knowledge />
-    <Project/>
+    <App />
   </React.StrictMode>
 );
 
-const footer = ReactDOM.createRoot(document.getElementById('footer'));
-footer.render(
-  <React.StrictMode>
-      <div className="thank text-center">
-        <h1 className='white' ><i className="bi bi-emoji-smile"></i> Thank You <i className="bi bi-emoji-smile"></i></h1>
-      </div>
-      <div className="footer">
-        <span>&copy; 2022 {name}</span>
-      </div>
-  </React.StrictMode>
-);
+reportWebVitals();
