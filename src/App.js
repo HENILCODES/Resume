76a8b614545ff101ca71dec7378df8fde08b2c5c
henilcode <henilcode@gmail.com>
2022-12-09 18:55:22 +0530

    bug fixed and move logical data in app compinents

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
-import React from "react";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
+import MessageBox from "./components/MessagesBox";
 import InputChatBox from "./components/InputChatBox";
 import MessageContext from "./components/templates/MessageContext";
-import "./App.css";
 
 function App() {
+  let [apiData, getMessageApi] = useState(false);
+  let dataSendHandler = async (ChatData) => {
+    console.log("data Send start");
+    await fetch(
+      "https://henil-2bfd8-default-rtdb.firebaseio.com/liveChat.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ChatData),
+        headers: { "content-type": "application/json" },
+      }
+    ).catch((Error) => {
+      console.log(Error.message);
+    });
 
+    console.log("data Send end", ChatData);
+    dataGetHandler();
+  };
+
+  let dataGetHandler = async () => {
+    console.log("get");
+    let response = await fetch(
+      "https://henil-2bfd8-default-rtdb.firebaseio.com/liveChat.json"
+    );
+    let data = await response.json();
+    getMessageApi(Object.values(data).reverse());
+    console.log("end get");
+  };
+useEffect(()=>{
+  dataGetHandler();
+},[])
   return (
     <div className="header">
       <div className="main">
-        <MessageContext.Provider value={{senderNameContext:'Henil',sendChatToContext:'s'}}>
+        <MessageContext.Provider
+          value={{ senderNameContext: "Henil", sendChatToContext: "s" }}
+        >
           <Header userName="Henil" />
-          <InputChatBox userName="Henil"/>
+          <div className="chats" id="chat_s">
+            {apiData &&
+              apiData.map((element, index) => {
+                return (
+                  <MessageBox
+                    key={index}
+                    senderName={element.sender}
+                    senderChat={element.chat}
+                    senderTime={element.sendTime}
+                  />
+                );
+              })}
+          </div>
+          <InputChatBox userName="Henil" setsendData={dataSendHandler} />
         </MessageContext.Provider>
       </div>
     </div>
--- src/components/InputChatBox.js
+++ src/components/InputChatBox.js
@@ -1,16 +1,14 @@
 import React, { memo, useState } from "react";
-import ChatBox from "./ChatBox";
 
 function InputChatBox(props) {
   const [chat, setChat] = useState({ chat: "" });
-  let [sendData, setsendData] = useState({});
-
+ 
   let submitHandler = (event) => {
     event.preventDefault();
     if (chat.chat.length === 0) {
       return;
     }
-    setsendData({
+    props.setsendData({
       ...chat,
       sender: props.userName,
       sendTime: new Date().toLocaleString("default"),
@@ -23,26 +21,23 @@ function InputChatBox(props) {
   };
 
   return (
-    <>
-      <ChatBox datasend={sendData} />
-      <div className="bottom">
-        <div id="chat_in">
-          <form className="form" onSubmit={submitHandler}>
-            <input
-              type="text"
-              value={chat.chat}
-              onChange={inputChatHandler}
-              className="input"
-              id="ChatsBox"
-              autoComplete="off"
-              placeholder="Type Message"
-              title="Type Message"
-            />
-            <button className="send bi bi-send"> </button>
-          </form>
-        </div>
+    <div className="bottom">
+      <div id="chat_in">
+        <form className="form" onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={chat.chat}
+            onChange={inputChatHandler}
+            className="input"
+            id="ChatsBox"
+            autoComplete="off"
+            placeholder="Type Message"
+            title="Type Message"
+          />
+          <button className="send bi bi-send"> </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
 
