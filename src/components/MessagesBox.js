cf3906e37354d643dc15b0c127bf02dba1022d8b
henilcode <henilcode@gmail.com>
2023-01-01 21:30:36 +0530

    temp bug commit but not set

--- src/components/MessagesBox.js
+++ src/components/MessagesBox.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function MessageBox(props) {
- 
+  let [optionActive, setOptionActive] = useState(false);
+
+  let optionClickHandler = () => {
+    setOptionActive(true);
+    // console.log(optionActive);
+  };
+  let optionCloseHandler = ()=>{
+    setOptionActive(false);
+  }
+  let copyHandler = ()=>{
+    console.log(props.senderChat);
+    optionCloseHandler();
+  }
   return (
-    <div className="message">
-      <div className="user">
-        <span>{props.senderName}</span>
+    <>
+      <div className="message">
+        <div className="user">
+          <span>{props.senderName}</span>
+          <span className="bi bi-chevron-down" onMouseEnter={optionClickHandler} onMouseLeave={optionCloseHandler}>
+          {optionActive && (
+            <ul className="option_message">
+              <li onClick={copyHandler}>Copy</li>
+            </ul>
+          )}
+          </span>
+        </div>
+        <div className="userchat">
+          <span> {props.senderChat} </span>
+        </div>
+        <div className="time">
+          <span>{props.senderTime}</span>
+        </div>
       </div>
-      <div className="userchat">
-        <span> {props.senderChat} </span>
-      </div>
-      <div className="time">
-        <span>{props.senderTime}</span>
-      </div>
-    </div>
+    </>
   );
 }
 
